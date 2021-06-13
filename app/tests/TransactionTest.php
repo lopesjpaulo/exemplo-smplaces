271aa13d92349aa9314d9b28a4dddf7cae2b95be
João Paulo Lopes da Silva <joaolopes@movida.com.br>
2021-06-13 15:41:08 -0300

    ajustes na imagem

--- app/tests/TransactionTest.php
+++ app/tests/TransactionTest.php
@@ -9,7 +9,12 @@ class TransactionTest extends TestCase
      */
     public function testTransaction()
     {
-        $this->json('POST', '/transactions', ['amount' => 10.01], ['HTTP_Authorization' => env('ACCEPTED_KEYS')]);
+        $this->json('POST', '/transactions', ['amount' => 10.01], 
+            [
+                'HTTP_Authorization' => env('ACCEPTED_KEYS'),
+                'Content-Type' => 'application/json'
+            ]
+        );
         $this->seeStatusCode(200);
     }
 }
